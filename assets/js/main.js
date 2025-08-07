54267cd8190e17ee1682b7ea68d027d42bf4f22e
Tibe Calderon <tibecvp@gmail.com>
2025-08-07 14:21:54 -0400

    Updates the footer section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*====== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*====== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
@@ -34,7 +34,7 @@ const blurHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the blur-header calss to the header tag
     this.scrollY >= 50 ? header.classList.add('blur-header')
-                       : header.classList.remove('blur-header')
+        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
@@ -69,12 +69,12 @@ window.addEventListener('scroll', blurHeader)
 
 // contactForm.addEventListener('submit', sendEmail)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
     // When the scrill is higher than 350 viewport height, add the show-scroll calss to the tag with the scrollup
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-                        : scrollUp.classList.remove('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -86,11 +86,11 @@ const scrollActive = () => {
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop -58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link')
         } else {
             sectionsClass.classList.remove('active-link')
@@ -109,7 +109,14 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data, .home__social, .contact__container`)
-sr.reveal(`.home__image, .footer__container`, {origin: 'bottom'})
-sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
-sr.reveal(`.about__image, .skills__content`, {origin: 'right'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+sr.reveal(`.home__image, .footer__container`, { origin: 'bottom' })
+sr.reveal(`.about__data, .skills__data`, { origin: 'left' })
+sr.reveal(`.about__image, .skills__content`, { origin: 'right' })
+sr.reveal(`.services__card, .projects__card`, { interval: 100 })
+
+/*=============== COPYRIGHT ===============*/
+const copyrightText = document.getElementById('footer__copyright')
+const currentYear = new Date().getFullYear()
+copyrightText.innerHTML = `
+    &copy; ${currentYear} Tiberio Calderón | Full-Stack Developer
+    <br><span>Template design by Bedimcode</span>`
